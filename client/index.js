bbeae317d22af33f8487490ae33c9c72293fce09
nrgwsth <anuragawasthi13@gmail.com>
2018-08-02 01:13:30 +0530

    install new husky version

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
-import React, {Component} from "react"
-import {render} from "react-dom"
-import {Provider} from "react-redux"
-import {Router} from "react-router-dom"
+import React, { Component } from "react"
+import { render } from "react-dom"
+import { Provider } from "react-redux"
+import { Router } from "react-router-dom"
 
 import history from "./history"
 import configureStore from "./store/configureStore"
@@ -10,11 +10,10 @@ import App from "./containers/App.js"
 const store = configureStore()
 
 render(
-	<Provider store = {store}>
-		<Router history = {history}>
+	<Provider store={store}>
+		<Router history={history}>
 			<App />
 		</Router>
 	</Provider>,
 	document.getElementById("app")
 )
-
